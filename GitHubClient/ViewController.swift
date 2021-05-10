16bd33aea0352e9d03905d66f1e551e33ef2464e
branch10480 <toshiharu.imaeda@gmail.com>
2021-05-10 22:42:50 +0900

    Initial Commit

---
+++ GitHubClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubClient
+//
+//  Created by branch10480 on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
